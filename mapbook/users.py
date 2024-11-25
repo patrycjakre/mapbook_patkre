6ea3b2b96933c3a7c36388625e8b9506791838d9
Czerubin <patrycja.cherubin@student.wat.edu.pl>
2024-11-25 14:54:32 +0100

    feat: full user list add

--- mapbook/users.py
+++ mapbook/users.py
@@ -9,4 +9,9 @@ users: list = [
     {'name': 'Julka', 'posts': 15, 'city': 'Wrocław'},
     {'name': 'Aleksandra', 'posts': 17, 'city': 'Zamość'},
     {'name': 'Patrycja', 'posts': 19, 'city': 'Szczecin'},
+    {'name': 'Patrycja', 'posts': 21, 'city': 'Olsztyn'},
+    {'name': 'Kinga', 'posts': 23, 'city': 'Szczecin'},
+    {'name': 'Dominik', 'posts': 25, 'city': 'Szczecin'},
+    {'name': 'Michał', 'posts': 27, 'city': 'Szczecin'},
+    {'name': 'Żerom', 'posts': 29, 'city': 'Szczecin'},
 ]
