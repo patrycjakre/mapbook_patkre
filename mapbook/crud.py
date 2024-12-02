06445c9ef15f5d60176dc6f6e1b0548fb6c59b64
patrycjakre <patrycja.krecisz@student.wat.edu.pl>
2024-12-02 12:58:08 +0100

    feat: add create new user

--- mapbook/crud.py
+++ mapbook/crud.py
@@ -7,3 +7,11 @@ def hello(user:str)->None:
 def read_users(users:list)->None :
     for user in users:
         print(f'twój znajomy {user['name']}, {user['city']} opublikował {user['posts']} postów')
+
+
+def add_user(userlist:list)->None:
+    new_name:str=input('prosze podać imię nowego znajomego ')
+    new_post:int=(input('podaj liczbę postów '))
+    new_city:str=input('podaj miasto ')
+    new_user: dict = {'name': new_name, 'posts': new_post, 'city': new_city}
+    userlist.append(new_user)
