27682cbe3622dba91c933afdf2df91c36134b0c3
patrycjakre <patrycja.krecisz@student.wat.edu.pl>
2024-12-09 12:21:35 +0100

    feat: menu add

--- main.py
+++ main.py
@@ -4,11 +4,32 @@ from mapbook.crud import hello, read_users, add_user, remove_user, update_user
 
 def main():
     hello(users[0]['name'])
-    read_users(users)
-    # add_user(users)
-    # remove_user(users)
-    update_user(users)
-    read_users(users)
+    while True:
+        print('======MENU======')
+        print('0. wyjście z programu')
+        print('1. dodaj znajomego')
+        print('2. wczytaj znajomego')
+        print('3. aktualizuj znajomego')
+        print('4. usuń znajomego')
+        print('5. generuj mapę z lokalizacją znajomego')
+        print('6. generuj mapę z lokalizacją wszystkich znajomych')
+
+        menu_option: str = input('użytkowniku wybierz opcję menu: ')
+        print(f'uzytkownik wybrał {menu_option}')
+        if menu_option == '0':
+            break
+        if menu_option == '1':
+            add_user(users)
+        if menu_option == '2':
+            read_users(users)
+        if menu_option == '3':
+            update_user(users)
+        if menu_option == '4':
+            remove_user(users)
+        if menu_option == '5':
+            pass
+        if menu_option == '6':
+            pass
 
 
 if __name__ == '__main__':
